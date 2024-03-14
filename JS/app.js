d615f59ae898f34888bf46dd449dca4ec445eb4f
Md Yeasin <mdyeasin234098@gmail.com>
2024-03-14 23:33:10 +0600

    latest post added

--- JS/app.js
+++ JS/app.js
@@ -3,7 +3,7 @@ const loadPost = async ()=>{
     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
     const data = await res.json();
     const posts = data.posts;
-    console.log(posts);
+    // console.log(posts);
     displayPost(posts);
 }
 
@@ -28,7 +28,7 @@ const displayPost = posts =>{
             <p class="mt-4">${post.description}</p>
             <hr class=" border border-dashed">
               <div class="mt-5 flex justify-between ">
-                <div class="flex gap-5">
+                <div class="flex gap-5 justify-between">
                   <p class="flex gap-3"><img src="images/comment.png" alt=""> ${post.comment_count}</p>
                 <p class="flex gap-3"><img src="images/view.png" alt="">${post.view_count}</p>
                 <p class="flex gap-3"><img src="images/clock.png" alt=""> ${post.posted_time} min</p>
@@ -50,14 +50,32 @@ const loadCard = async ()=>{
   displayCard(cards);
 }
 
+const latestCardArea = document.getElementById('card');
 
 
 const displayCard= cards =>{
  console.log(cards);
  cards?.forEach(card =>{
   console.log(card);
+  const latestCard = document.createElement('div');
+  latestCard.classList=``;
+  latestCard.innerHTML= `
+  <div class="card w-96 bg-base-100 shadow-xl">
+          <figure><img src="${card.cover_image}" alt="Shoes" /></figure>
+          <div class="card-body">
+            <div class="flex gap-2"><img class="w-[24px] h-[24px]" src="images/calendar.png" alt=""> <p>${card.author.posted_date}</p></div>
 
-
+            <h2 class="card-title font-bold">${card.title}</h2>
+            <p>${card.description} </p>
+            <div class="card-actions flex ">
+              <div class="w-[40px] h-[40px] "><img  src="${card.profile_image}" alt=""></div>
+              <div><p>Cameron Williamson</p>
+              <p>Unknown</p></div>
+            </div>
+          </div>
+        </div>
+  `  
+  latestCardArea.appendChild(latestCard);
  })
 }
 
